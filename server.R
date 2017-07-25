8a44233b334619a0392f48a89c253829693188b8
plinders <plinders@Peters-MacBook-Pro-2.local>
2017-07-25 21:15:21 +0200

    debugging pt 82

--- server.R
+++ server.R
@@ -1,9 +1,9 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
@@ -15,16 +15,16 @@ library(rJava)
 library(RBioFormats)
 library(parallel)
 library(data.table)
-library(log4r)
+#library(log4r)
 options(shiny.maxRequestSize = 1024 * 1024 ^ 2)
 
-loggerDebug <- create.logger()
-logfile(loggerDebug) <- 'debugData.log'
-level(loggerDebug) <- 'INFO'
+#loggerDebug <- create.logger()
+#logfile(loggerDebug) <- 'debugData.log'
+#level(loggerDebug) <- 'INFO'
 
-loggerServer <- create.logger()
-logfile(loggerServer) <- 'serverData.log'
-level(loggerServer) <- 'INFO'
+#loggerServer <- create.logger()
+#logfile(loggerServer) <- 'serverData.log'
+#level(loggerServer) <- 'INFO'
 
 # Define server logic required for ColocalizeR
 shinyServer(function(input, output, session) source("src/app.R", local = TRUE))
--- src/app.R
+++ src/app.R
@@ -11,16 +11,16 @@ observeEvent(input$run, {
   #remove leftover .zip(s) and other files first
   file.remove(list.files(pattern = ".zip$|.lif$|.pdf$|.txt$"))
   inFile <- input$file1
-  
+
   if (is.null(inFile))
     return(NULL)
 
-  
+
   #ColocalizeR starts here
   #set up parallel computing for image reading optimization
   total_cores <- detectCores() - 1
   cl <- makeCluster(total_cores)
-  
+
   #set bools to false so they don't break the script
   oif_bool <- FALSE
   lif_bool <- FALSE
@@ -43,8 +43,8 @@ observeEvent(input$run, {
     input_files <- paste(input_dir, "/", dir(input_dir, pattern = ".oif$|.lif$|.lsm$|.tif$"), sep = "")
     withProgress(message = "Reading images", {
       if (sum(grepl(".oif$", input_files)) / length(input_files) == 1) {
-       # images_list <- parLapply(cl, input_files, read.image)
-        images_list <- lapply(input_files, read.image)
+        images_list <- parLapply(cl, input_files, read.image)
+        #images_list <- lapply(input_files, read.image)
         oif_meta <- list()
         for (i in seq_along(images_list)) {
           oif_meta[[i]] <- globalMetadata(images_list[[i]])
@@ -68,8 +68,8 @@ observeEvent(input$run, {
         lsm_bool <- TRUE
         showNotification("Import complete, .lsm files detected. Starting processing...", type = "message")
       } else if (sum(grepl(".tif$", input_files)) / length(input_files) == 1) {
-       # images_list <- parLapply(cl, input_files, read.image)
-        images_list <- lapply(input_files, read.image)
+        images_list <- parLapply(cl, input_files, read.image)
+        #images_list <- lapply(input_files, read.image)
         tif_meta <- list()
         for (i in seq_along(images_list)) {
           tif_meta[[i]] <- basename(file_path_sans_ext(input_files[[i]]))
@@ -82,11 +82,11 @@ observeEvent(input$run, {
     })
   }
 
-  
+
   #save fluorochrome info to variables
   fluo1 <- input$selectfluor1
   fluo2 <- input$selectfluor2
-  
+
   #make progress object to track progress
   progress <- shiny::Progress$new()
   on.exit(progress$close())
@@ -100,9 +100,9 @@ observeEvent(input$run, {
       require("tools")
       require("data.table")
       require("shiny")
-      
+
       progress$inc(1/(length(images_list)/total_cores))
-      
+
       if (oif_bool == TRUE) {
         img_name <- file_path_sans_ext(oif_meta[[i]]$`[File Info] DataName`)
       } else if (lif_bool == TRUE) {
@@ -112,13 +112,13 @@ observeEvent(input$run, {
       } else if (tif_bool == TRUE) {
         img_name <- tif_meta[[i]]
       }
-      
+
       #Start pdf, plots will go in here
       pdf(paste(input_dir, "_", i, "_", img_name, ".pdf", sep = ""), paper = "a4")
-      
+
       chan1_dt <- data.table(getFrame(images_list[[i]], as.numeric(input$selectchan1)))
       chan2_dt <- data.table(getFrame(images_list[[i]], as.numeric(input$selectchan2)))
-      
+
       #Change pixel intensity scaling based on checkbox values
       if (input$bitdepth == 1){
         chan1_dt_16bit <- chan1_dt * 256
@@ -130,40 +130,40 @@ observeEvent(input$run, {
         chan1_dt_16bit <- chan1_dt * 65536
         chan2_dt_16bit <- chan2_dt * 65536
       }
-      
+
       #subtract 10000 from Airyscan processing images
       if (input$airyscan == 1){
         chan1_dt_16bit <- chan1_dt_16bit - 10000
         chan2_dt_16bit <- chan2_dt_16bit - 10000
       }
-      
+
       #Change pixel intensity scale from 0 to 1 to 0 to 4096
       # if (max(chan1_dt, na.rm = TRUE) <= 1) {
       #   chan1_dt_16bit <- chan1_dt * 4096
       #   chan2_dt_16bit <- chan2_dt * 4096
       # }
-      
+
       #Plot histograms to show distributions of intensities
       #Data frames cannot be plotted as histograms, hence as.matrix
       hist(as.matrix(chan1_dt_16bit), main = "Pre-normalization", xlab = paste(fluo1, "intensity"))
       hist(as.matrix(chan2_dt_16bit), main = "Pre-normalization", xlab = paste(fluo2, "intensity"))
-      
+
       #Scatter plot of un-normalized data
       #Scatter plot takes numeric vectors as input, this line also converts the data frames
       smoothScatter(as.vector(as.matrix(chan1_dt_16bit)), as.vector(as.matrix(chan2_dt_16bit)), main = "Pre-normalization", xlab = fluo1, ylab = fluo2)
-      
+
       #Convert data frames to vectors for further processing
       chan1_vect <- as.vector(as.matrix(chan1_dt_16bit))
       chan2_vect <- as.vector(as.matrix(chan2_dt_16bit))
-      
+
       #Exclude pixels that are < 20% of max in both channels
       chan1_vect[chan1_vect < (0.20 * max(chan1_vect)) & chan2_vect < (0.20 * max(chan2_vect))] <- NA
-      
+
       #Plot new histograms
       hist(chan1_vect, main = "Filter < 200 AU", xlab = paste(fluo1, "intensity"))
       hist(chan2_vect, main = "Filter < 200 AU", xlab = paste(fluo2, "intensity"))
-      
-      #Normalize channels based on mean fluorescent intensity 
+
+      #Normalize channels based on mean fluorescent intensity
       #Plot histograms, scatter plot, regression line (-1 to force line through origin)
       if (input$normalizevals == 1){
         chan1_relative <- chan1_vect / mean(chan1_vect, na.rm = TRUE)
@@ -180,16 +180,16 @@ observeEvent(input$run, {
         smoothScatter(chan1_relative, chan2_relative, main = "Pre-Y axis transformation", xlab = fluo1, ylab = fluo2)
         abline(lm(chan2_relative ~ chan1_relative - 1), col = "red")
       }
-    
+
       #Calculate how Y-axis (mch_trans) needs to be transformed to reach X=Y
       regression_norm <- lm(chan2_relative ~ chan1_relative - 1)
       chan2_trans <- chan2_relative * (1 / regression_norm$coefficients[1])
-      
+
       #Place both vectors in data frame, plot new scatter plot
       scatter_dt <- data.table(chan1_relative, chan2_trans)
       smoothScatter(scatter_dt, xlim = c(0, max(scatter_dt$chan1_relative, na.rm = TRUE)), ylim = c(0, max(scatter_dt$chan2_trans, na.rm = TRUE)), main = "Final", xlab = fluo1, ylab = fluo2)
       abline(lm(chan2_trans ~ chan1_relative - 1, data = scatter_dt), col = "red")
-      
+
       #Calculate fineal linear regression
       final_model <- lm(chan2_trans ~ chan1_relative - 1, data = scatter_dt)
       dev.off()
@@ -206,7 +206,7 @@ observeEvent(input$run, {
     lapply(seq_along(images_list), colocmainloop)
 
     stopCluster(cl)
-    
+
     #zip all files
     to_zip <- list.files(pattern = paste(input_dir, "..", sep = ""), include.dirs = FALSE, recursive = FALSE)
     zip(paste(file_path_sans_ext(input_dir), "_output", sep = ""), files = to_zip)
@@ -215,21 +215,21 @@ observeEvent(input$run, {
       filename <- function() {
         paste(file_path_sans_ext(input_dir), "_output", ".zip", sep = "")
       },
-      
+
       content <- function(file) {
         file.copy(paste(file_path_sans_ext(input_dir), "_output", ".zip", sep = ""), file)
       },
       contentType = "application/zip"
     )
-    
-    
+
+
     session$sendCustomMessage("download_ready", list(fileSize = floor(runif(1) * 10000)))
-    
+
     #clean up files
     file.remove(to_zip)
     unlink(input_dir, recursive = TRUE)
     unlink("__MACOSX", recursive = TRUE)
-    
-    
+
+
 
 })
